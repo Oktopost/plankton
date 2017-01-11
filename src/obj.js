6c96cf80e50f3fb6ef2b3a255104d131ca6ac3f5
Alexey Puchkov <alexey@oktopost.com>
2017-01-11 07:11:13 +0200

    Add any method to obj module

--- src/obj.js
+++ src/obj.js
@@ -1,18 +1,74 @@
 'use strict';
 
 
+const is = require('./is');
+
+
+/**
+ * @param subject
+ * @returns {*|undefined}
+ */
+var any = function(subject) {
+	var key = any.key(subject);
+	return (is.defined(key) ? subject[key] : undefined);
+};
+
+/**
+ * @param {Object} subject
+ * @return {*|undefined}
+ */
+any.value = any;
+
+/**
+ * @param {Object} subject
+ * @return {*|undefined}
+ */
+any.key = function(subject) {
+	var keys = obj.keys(subject);
+	return keys.length > 0 ? keys[0] : undefined;
+};
+
+/**
+ * @param {Object} subject
+ * @return {*|undefined}
+ */
+any.item = function(subject) {
+	var key = any.key(subject);
+	var obj = undefined;
+	
+	if (is.defined(key)) {
+		var obj = {};
+		obj[key] = subject[key];
+	}
+	
+	return obj;
+};
+
+
 var obj = {
+	any: any,
+	
 
 	/**
 	 * @param {Object} subject
 	 * @returns {Array}
 	 */
 	values: function(subject) {
-		return Object.keys(subject).reduce(function (result, key) {
+		return obj.keys(subject).reduce(function (result, key) {
 			result.push(subject[key]);
 			return result;
 		}, [])
-	}
+	},
+	
+	/**
+	 * @param {Object} subject
+	 * @returns {Array}
+	 */
+	keys: function(subject) {
+		return Object.keys(subject);
+	},
+	
+	
 };
 
 
--- test/src/obj.js
+++ test/src/obj.js
@@ -30,4 +30,74 @@ suite('obj module', function() {
 			assert.deepEqual([2], obj.values(a));
 		});
 	});
+	
+	
+	suite('obj.keys', () => {
+		test('empty object return array', () => {
+			assert.isArray(obj.keys({}));
+		});
+		
+		test('empty object empty keys', () => {
+			assert.deepEqual([], obj.keys({}));
+		});
+		
+		test('object with keys will return keys', () => {
+			assert.deepEqual(['a', '2'].sort(), obj.keys({'a': 'b', 2: 3}).sort());
+		});
+		
+		test('object with prototype values does not return prototype values', function() {
+			var foo = function() {};
+			foo.prototype.c = 1;
+			
+			var a = new foo;
+			a.b = 2;
+			
+			assert.deepEqual(['b'], obj.keys(a));
+		});
+	});
+	
+	
+	suite('obj.any', () => {
+		suite('obj.any.value', () => {
+			test('empty object return undefined', () => {
+				assert.isUndefined(obj.any.value({}));
+			});
+			
+			test('object with one key return a value', () => {
+				assert.equal(1, obj.any.value({'a': 1}));
+			});
+			
+			test('object with keys return a value', () => {
+				assert.include(['a', 2], obj.any.value({'c': 2, 'b': 'a'}));
+			});
+		});
+		
+		suite('obj.any.key', () => {
+			test('empty object return undefined', () => {
+				assert.isUndefined(obj.any.key({}));
+			});
+			
+			test('object with one key return a key', () => {
+				assert.equal('a', obj.any.key({'a': 1}));
+			});
+			
+			test('object with keys return a key', () => {
+				assert.include(['a', 'b'], obj.any.key({'a': 2, 'b': 'c'}));
+			});
+		});
+		
+		suite('obj.any.item', () => {
+			test('empty object return undefined', () => {
+				assert.isUndefined(obj.any.item({}));
+			});
+			
+			test('object with element, element returned', () => {
+				assert.deepEqual({'a': 1}, obj.any.item({'a': 1}));
+			});
+			
+			test('object with number of items', () => {
+				assert.include([{'a': 2}, {'b': 'c'}], obj.any.item({'a': 2, 'b': 'c'}));
+			});
+		});
+	});
 });
