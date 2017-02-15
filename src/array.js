190125233e028a3821a03fece570d74e563fb1fc
Alexey Puchkov <alexey@oktopost.com>
2017-02-15 11:34:19 +0200

    Add last, first, count, isNormalized and normalize methods to array

--- src/array.js
+++ src/array.js
@@ -4,6 +4,10 @@
 var is = require('./is');
 
 
+/**
+ * @param {*} subject
+ * @return {Array}
+ */
 var array = function(subject) {
 	if (is.undefined(subject)) {
 		return [];
@@ -13,7 +17,7 @@ var array = function(subject) {
 };
 
 /**
- * @param {Object} subject
+ * @param {Array} subject
  * @param {function(*)} callback
  */
 array.forEach = function(subject, callback) {
@@ -23,14 +27,14 @@ array.forEach = function(subject, callback) {
 };
 
 /**
- * @param {Object} subject
+ * @param {Array} subject
  * @param {function(*)} callback
  */
 array.forEach.value = array.forEach;
 
 /**
- * @param {Object} subject
- * @param {function(number)} callback
+ * @param {Array} subject
+ * @param {function(Number)} callback
  */
 array.forEach.key = function(subject, callback) {
 	for (var key in subject) {
@@ -45,8 +49,8 @@ array.forEach.key = function(subject, callback) {
 };
 
 /**
- * @param {Object} subject
- * @param {function(number *)} callback
+ * @param {Array} subject
+ * @param {function(Number *)} callback
  */
 array.forEach.pair = function(subject, callback) {
 	array.forEach.key(subject, function(key) {
@@ -55,8 +59,8 @@ array.forEach.pair = function(subject, callback) {
 };
 
 /**
- * @param {Object} subject
- * @param {function(Object)} callback
+ * @param {Array} subject
+ * @param {function(Array)} callback
  */
 array.forEach.item = function(subject, callback) {
 	array.forEach.pair(subject, function(key, value) {
@@ -64,6 +68,107 @@ array.forEach.item = function(subject, callback) {
 		obj[key] = value;
 		return callback(obj);
 	});
+};
+
+
+/**
+ * @param {Array} subject
+ * @return {*}
+ */
+array.last = function (subject) {
+	if (subject.length === 0) {
+		return undefined;
+	}
+	
+	return subject[subject.length - 1];
+};
+
+/**
+ * @param {Array} subject
+ * @return {Number|undefined}
+ */
+array.last.key = function (subject) {
+	if (subject.length === 0) {
+		return undefined;
+	}
+	
+	return subject.length - 1;
+};
+
+/**
+ * @param {Array} subject
+ * @return {*}
+ */
+array.last.value = array.last;
+
+
+/**
+ * @param {Array} subject
+ * @return {*}
+ */
+array.first = function (subject) {
+	var first = undefined;
+	
+	array.forEach.value(subject, function(value) {
+		first = value;
+		return false;
+	});
+	
+	return first;
+};
+
+/**
+ * @param {Array} subject
+ * @return {Number|undefined}
+ */
+array.first.key = function (subject) {
+	var first = undefined;
+	
+	array.forEach.key(subject, function(value) {
+		first = value;
+		return false;
+	});
+	
+	return first;
+};
+
+/**
+ * @param {Array} subject
+ * @return {*}
+ */
+array.first.value = array.first;
+
+
+/**
+ * @param {Array} subject
+ * @returns {Number}
+ */
+array.count = function (subject) {
+	var count = 0;
+	array.forEach(subject, function() { count++; });
+	return count;
+};
+
+/**
+ * @param {Array} subject
+ * @returns {Number}
+ */
+array.isNormalized = function (subject) {
+	return subject.length === 0 || array.last.key(subject) === (array.count(subject) - 1);
+};
+
+/**
+ * @param {Array} subject
+ * @returns {Number}
+ */
+array.normalize = function (subject) {
+	var arr = [];
+	
+	array.forEach(subject, function(value) {
+		arr.push(value);
+	});
+	
+	return arr;
 };
 
 
--- test/src/array.js
+++ test/src/array.js
@@ -149,6 +149,176 @@ suite('array module', function() {
 				assert.deepEqual([[{'0': 1}], [{'100': 2}]], result);
 			});
 		});
+	});
+	
+	
+	suite('array.last', () => {
 		
+		test('array.last.value is same', () => {
+			assert.equal(array.last, array.last.value);
+		});
+		
+		
+		suite('array.last.value', () => {
+			
+			test('Empty array return undefined', () => {
+				assert.isUndefined(array.last.value([]));
+			});
+			
+			test('Last value returned', () => {
+				assert.equal('a', array.last.value(['a']));
+			});
+			
+			test('Offseted index last value still returned', () => {
+				var arr = [];
+				arr[400] = 'a';
+				arr[500] = 'b';
+				
+				assert.equal('b', array.last.value(arr));
+			});
+		});
+		
+		
+		suite('array.last.key', () => {
+			
+			test('Empty array return undefined', () => {
+				assert.isUndefined(array.last.key([]));
+			});
+			
+			test('Last key returned', () => {
+				assert.equal(0, array.last.key(['a']));
+			});
+			
+			test('Offseted index last value still returned', () => {
+				var arr = [];
+				arr[400] = 'a';
+				arr[500] = 'b';
+				
+				assert.equal(500, array.last.key(arr));
+			});
+		});
+	});
+	
+	suite('array.first', () => {
+		
+		test('array.first.value is same', () => {
+			assert.equal(array.first, array.first.value);
+		});
+		
+		
+		suite('array.first.value', () => {
+			
+			test('empty array return undefined', () => {
+				assert.isUndefined(array.first.value([]));
+			});
+			
+			test('first value returned', () => {
+				assert.equal('a', array.first.value(['a']));
+			});
+			
+			test('offseted index first value still returned', () => {
+				var arr = [];
+				arr[400] = 'a';
+				arr[500] = 'b';
+				
+				assert.equal('a', array.first.value(arr));
+			});
+		});
+		
+		
+		suite('array.first.key', () => {
+			
+			test('empty array return undefined', () => {
+				assert.isUndefined(array.first.key([]));
+			});
+			
+			test('first key returned', () => {
+				assert.equal(0, array.first.key(['a']));
+			});
+			
+			test('offseted index first value still returned', () => {
+				var arr = [];
+				arr[400] = 'a';
+				arr[500] = 'b';
+				
+				assert.equal(400, array.first.key(arr));
+			});
+		});
+	});
+	
+	
+	suite('array.count', () => {
+		
+		test('empty array', () => {
+			assert.equal(0, array.count([]));
+		});
+		
+		test('valid array', () => {
+			assert.equal(2, array.count(['a', 'b']));
+		});
+		
+		test('array with offset values', () => {
+			var arr = [];
+			arr[400] = 'a';
+			arr[500] = 'b';
+			
+			assert.equal(2, array.count(arr));
+		});
+	});
+	
+	
+	suite('array.isNormalized', () => {
+		
+		test('empty array', () => {
+			assert.isTrue(array.isNormalized([]));
+		});
+		
+		test('valid array', () => {
+			assert.isTrue(array.isNormalized(['a', 'b']));
+		});
+		
+		test('array with offset values', () => {
+			var arr = [];
+			
+			arr[400] = 'a';
+			arr[500] = 'b';
+			
+			assert.isFalse(array.isNormalized(arr));
+		});
+	});
+	
+	
+	suite('array.normalize', () => {
+		
+		test('empty array', () => {
+			assert.deepEqual([], array.normalize([]));
+		});
+		
+		test('valid array', () => {
+			assert.deepEqual(['a', 'b'], array.normalize(['a', 'b']));
+		});
+		
+		test('array with offset values', () => {
+			var arr = [];
+			
+			arr[400] = 'a';
+			arr[500] = 'b';
+			
+			assert.deepEqual(['a', 'b'], array.normalize(arr));
+		});
+		
+		test('original array not modified', () => {
+			var arr = [];
+			var original = [];
+			
+			arr[10] = 'a';
+			arr[12] = 'b';
+			original[10] = 'a';
+			original[12] = 'b';
+			
+			array.normalize(arr)
+			
+			assert.deepEqual(original, arr);
+		});
 	});
 });
