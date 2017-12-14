cd0df7ea76c8d267483762876447a3113b0c1fc1
Alexey Puchkov <alexey.pkv@gmail.com>
2017-12-14 16:56:25 +0200

    Add plankton.url to plankton

---
+++ src/Plankton/url.js
@@ -0,0 +1,137 @@
+namespace('Plankton', function(root)
+{
+	var is		= root.Plankton.is;
+	var foreach	= root.Plankton.foreach;
+	
+	
+	/**
+	 * @class Plankton.url
+	 * @alias url
+	 */
+	var url = {};
+
+
+	/**
+	 * @param {string|*} path
+	 * @param {{}=} params
+	 * @returns {string}
+	 */
+	url.encode = function (path, params)
+	{
+		var queryParams	= {};
+		var link		= path.toString();
+		var encodedLink	= '';
+		var addSlash	= false;
+		var queryParts	= [];
+		
+		foreach.pair(params, function (key, value)
+		{
+			if (is.bool(value))
+			{
+				params[key] = (value ? '1' : '0');
+			}
+		});
+		
+		foreach.pair(params, function (key, value)
+		{
+			if (link.indexOf('{' + key + '}') === -1)
+			{
+				queryParams[key] = value;
+				return;
+			}
+			
+			link = link.replace(new RegExp('{' + key + '}', 'g'), value.toString());
+		});
+		
+		foreach(link.split('/'), function (part)
+		{
+			if (addSlash)
+			{
+				encodedLink += '/';
+			}
+			else
+				{
+				addSlash = true;
+			}
+			
+			encodedLink += encodeURIComponent(part);
+		});
+		
+		if (!is(queryParams))
+		{
+			return encodedLink;
+		}
+		
+		foreach.pair(queryParams, function (key, value)
+		{
+			queryParts.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+		});
+		
+		return encodedLink + '?' + queryParts.join('&');
+	};
+
+	/**
+	 * @param {string} url
+	 * @returns {{ uri: string, path: string[], params: {} }}
+	 */
+	url.decode = function (url)
+	{
+		var data	= url.split('?');
+		var path	= [];
+		var params	= {};
+		var uri		= data[0];
+		
+		
+		if (data.length === 1)
+		{
+			data = [ data[0], '' ];
+		}
+		else if (data.length > 2)
+		{
+			data = [ data[0], data.splice(1).join('?') ];
+		}
+		
+		foreach(data[0].split('/'), function (pathPart)
+		{
+			if (pathPart.length !== 0)
+			{
+				path.push(decodeURIComponent(pathPart));
+			}
+		});
+		
+		foreach(data[1].split('&'), function (queryExpression)
+		{
+			var query	= queryExpression.split('=');
+			var key		= decodeURIComponent(query[0]);
+			var value;
+			
+			if (key.length === 0)
+			{
+				return;	
+			}
+			else if (query.length === 1)
+			{
+				value = '';
+			}
+			else if (query.length > 2)
+			{
+				value = decodeURIComponent(query.splice(1).join('='));
+			}
+			else
+			{
+				value = decodeURIComponent(query[1]);
+			}
+			
+			params[key] = value;
+		});
+		
+		return {
+			uri:	uri,
+			path:	path,
+			params:	params
+		};
+	};
+	
+	
+	this.url = url;
+});
---
+++ test/src/Plankton/url.js
@@ -0,0 +1,160 @@
+const url = require('../../index').url;
+const assert = require('chai').assert;
+
+
+suite('url module', () =>
+{
+	suite('encode', () =>
+	{
+		test('empty data, empty string returned', () =>
+		{
+			assert.equal('', url.encode('', {}));
+		});
+		
+		test('only path, path returned', () =>
+		{
+			assert.equal('a', url.encode('a', {}));
+		});
+		
+		test('path escaped', () =>
+		{
+			assert.equal('a%23b', url.encode('a#b', {}));
+		});
+		
+		test('slash at start persisted', () =>
+		{
+			assert.equal('/a', url.encode('/a', {}));
+		});
+		
+		test('slash at end persisted', () =>
+		{
+			assert.equal('a/', url.encode('a/', {}));
+		});
+		
+		test('slash persisted', () =>
+		{
+			assert.equal('a/b', url.encode('a/b', {}));
+		});
+		
+		test('double slash persisted', () =>
+		{
+			assert.equal('a//b', url.encode('a//b', {}));
+		});
+		
+		test('query param appended', () =>
+		{
+			assert.equal('a?b=3', url.encode('a', {b: 3}));
+		});
+		
+		test('query param value escaped', () =>
+		{
+			assert.equal('a?b=a%23b', url.encode('a', {b: 'a#b'}));
+		});
+		
+		test('query param name escaped', () => 
+		{
+			assert.equal('a?a%23b=a', url.encode('a', {'a#b': 'a'}));
+		});
+		
+		test('path param used only in path', () => 
+		{
+			assert.equal('a123c', url.encode('a{b}c', {'b': '123'}));
+		});
+		
+		test('path param escaped', () =>
+		{
+			assert.equal('a%23c', url.encode('a{b}c', {'b': '#'}));
+		});
+		
+		test('path param usend a number of times', () =>
+		{
+			assert.equal('a1c/1', url.encode('a{b}c/{b}', {'b': '1'}));
+		});
+		
+		test('bool param parsed as int', () =>
+		{
+			assert.equal('1', url.encode('{b}', {'b': true}));
+			assert.equal('0', url.encode('{b}', {'b': false}));
+		});
+	});
+	
+	
+	suite('decode', () =>
+	{
+		test('empty string', () =>
+		{
+			assert.deepEqual({ uri: '', path: [], params: {}}, url.decode(''));
+		});
+		
+		test('path has slash only', () =>
+		{
+			assert.deepEqual({ uri: '/', path: [], params: {}}, url.decode('/'));
+		});
+		
+		test('path starts with slash', () =>
+		{
+			assert.deepEqual({ uri: '/abc', path: ['abc'], params: {}}, url.decode('/abc'));
+		});
+		
+		test('path end with slash', () =>
+		{
+			assert.deepEqual({ uri: 'abc/', path: ['abc'], params: {}}, url.decode('abc/'));
+		});
+		
+		test('long path', () =>
+		{
+			assert.deepEqual({ uri: '/abc/def', path: [ 'abc', 'def' ], params: {}}, url.decode('/abc/def'));
+		});
+		
+		test('path is decoded', () =>
+		{
+			assert.deepEqual({ uri: '/a%23b', path: [ 'a#b' ], params: {}}, url.decode('/a%23b'));
+		});
+		
+		test('query params', () =>
+		{
+			assert.deepEqual({ uri: '', path: [], params: { a: 'abc' }}, url.decode('?a=abc'));
+		});
+		
+		test('query params value decoded', () =>
+		{
+			assert.deepEqual({ uri: '', path: [], params: { a: 'ab#c' }}, url.decode('?a=ab%23c'));
+		});
+		
+		test('query params key decoded', () =>
+		{
+			assert.deepEqual({ uri: '', path: [], params: { 'a#b': 'c' }}, url.decode('?a%23b=c'));
+		});
+		
+		test('query param is empty', () =>
+		{
+			assert.deepEqual({ uri: '', path: [], params: { 'a': '' }}, url.decode('?a='));
+			assert.deepEqual({ uri: '', path: [], params: { 'a': '' }}, url.decode('?a'));
+		});
+		
+		test('query param value has equal sign', () =>
+		{
+			assert.deepEqual({ uri: '', path: [], params: { 'a': 'b=c' }}, url.decode('?a=b=c'));
+		});
+		
+		test('number of query params', () =>
+		{
+			assert.deepEqual({ uri: '', path: [], params: { 'a': 'b', 'c': 'd' }}, url.decode('?a=b&c=d'));
+		});
+		
+		test('path with query params', () =>
+		{
+			assert.deepEqual({ uri: '/a/b', path: ['a', 'b'], params: { 'c': 'd', 'e': 'f' }}, url.decode('/a/b?c=d&e=f'));
+		});
+		
+		test('query params have another ? sign', () =>
+		{
+			assert.deepEqual({ uri: '', path: [], params: { 'c': 'd?e' }}, url.decode('?c=d?e'));
+		});
+		
+		test('query params have the = sign not attached to any key', () =>
+		{
+			assert.deepEqual({ uri: '', path: [], params: { 'c': 'd' }}, url.decode('?=&c=d'));
+		});
+	});
+});
